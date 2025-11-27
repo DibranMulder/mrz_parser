1dd403936ac1b1b3f02daaeed7bda7de80cabc7d
Wouter Ensink <46427708+w-ensink@users.noreply.github.com>
2025-11-27 09:52:22 +0100

    export parser

--- lib/mrz_parser.dart
+++ lib/mrz_parser.dart
@@ -1,4 +1,5 @@
 export 'src/driving_licence_mrz_parser.dart';
 export 'src/mrz_exceptions.dart';
+export 'src/mrz_parser.dart';
 export 'src/mrz_result.dart';
 export 'src/passport_mrz_parser.dart';
